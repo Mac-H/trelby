14c4a9aab7f220f2dae0f221cbf798d772a6dc28
Anil Gulecha <anil.verve@gmail.com>
2011-11-29 21:14:24 +0530

    Fix windows build errors after the source move to 'src/'.

--- build.bat
+++ build.bat
@@ -1,5 +1,12 @@
+del /a /q winbuild
+mkdir winbuild
+xcopy src\* winbuild\
+xcopy * winbuild\
+xcopy /i resources winbuild\dist\resources
+
+cd winbuild
 python -OO setup.py py2exe
-xcopy /i resources dist\resources
+
 copy vcredist_x86.exe dist
 copy names.txt.gz dist
 copy sample.trelby dist
@@ -7,3 +14,10 @@ copy manual.html dist
 copy fileformat.txt dist
 copy LICENSE dist
 copy dict_en.dat.gz dist
+nsis.bat
+
+move /y setup*exe .. 
+move /y dist ..\dist
+
+cd ..
+del /a /q winbuild
